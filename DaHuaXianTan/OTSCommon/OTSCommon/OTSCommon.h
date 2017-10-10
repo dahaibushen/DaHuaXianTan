72ecadfdede0b9c0a221942ca6f18ebda1fc6670
huyiyong1 <huyiyong@jd.com>
2017-10-10 16:45:47 +0800

    提交代码

---
+++ DaHuaXianTan/OTSCommon/OTSCommon/OTSCommon.h
@@ -0,0 +1,19 @@
+//
+//  OTSCommon.h
+//  OTSCommon
+//
+//  Created by HU on 2017/10/10.
+//  Copyright © 2017年 HU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OTSCommon.
+FOUNDATION_EXPORT double OTSCommonVersionNumber;
+
+//! Project version string for OTSCommon.
+FOUNDATION_EXPORT const unsigned char OTSCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OTSCommon/PublicHeader.h>
+
+
